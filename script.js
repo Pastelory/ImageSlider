623dde01345bf3c82770fff00261f7a6f435ace9
~Pastelory~ <68802910+Pastelory@users.noreply.github.com>
2020-11-15 18:04:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function nextSlide(){
     }
     counter++;
 
-    container.style.backgroundImage = `url(image/image${counter}.jpeg)`
+    container.style.backgroundImage = `url(image/image${counter}.jpg)`
 }
 
 function prevSlide(){
@@ -24,5 +24,5 @@ function prevSlide(){
     }
     counter--;
 
-    container.style.backgroundImage = url(`url(image/image${counter}.jpeg)`)
+    container.style.backgroundImage = url(`url(image/image${counter}.jpg)`)
 }
